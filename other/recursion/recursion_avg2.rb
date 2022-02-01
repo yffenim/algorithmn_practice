91e41b698090104cc9647ea9b6a4b832bae5dc3e
E <effyverse@gmail.com>
2022-02-01 17:20:38 -0500

    refactor recursion for average of array

--- other/recursion/recursion_avg2.rb
+++ other/recursion/recursion_avg2.rb
@@ -1,54 +1,5 @@
-# A Redo of Calculating the Average Via Recursion
+# Find average of array of numbers using recursion
 
-# Using 10DIME:
-#
-# Input:
-# An array of integers.
-#
-# Output: 
-# The average of the array.
-# 
-# Definitions:
-# An average is the sum of all integers / the length of array
-#
-# Instructions (Pseudo-Code):
-# See below.
-#
-# Methods: N/A
-#
-# Edge Cases/Exceptions/Examples (tests):
-# What ii array is negative numbers?
-# What if array is empty?
-#
-# Solving with Recursion:
-# 1. BASE CASE: arr.count == 0
-# 2. The function for the recursion: avg = arr.sum/arr.count
-# 3. The next-to-last case: arr[0..count-2]
-# 4. Take out last case from the recursion line and represent this relationship:
-
-#   count = arr.count
-#   avg = arr.sum/count
-#   
-#   Taking out the last case and representing the relationship:
-#   avg_excl_last_num = arr[0..count-2]/count * arr[count-1]
-#   
-#   What we know: 
-#   - the count
-#   - the average excluding last arr item
-#   - the equation that the avg = sum / count
-#   - the value we don't have is the sum so let's try finding it with what we do have:
-#
-#   sum_excl_last_num =  avg_excl_last_num * count
-#
-#   - so now we have the sum_excl_last_num. 
-#   - What is our goal? Finding the average of all numbers. So the sum we need is the sum_excl_last_num + last_num (to give us the sum of all numbers) 
-#
-#   sum_of_all_num =  sum_excl_last_num + arr[count-1]
-#
-#   Then we find the average:
-#
-#   sum_of_all_num / count
-#
 def average(arr)
   # base case:
   count = arr.count
@@ -57,10 +8,18 @@ def average(arr)
   elsif count == 1
     return arr[0]
   else
-  # What do we kno?? 
+    # average = arr.sum / arr.count
+    # arr.sum = average * arr.count
+    # sum_excl_last_num = average(arr[0..count-2]) * (count-1)
+    
+    # sum_of_all_num = sum_excl_last_num + arr[count-1]
+    
+    # average = sum_of_all_num / count
 
+    # refactoring the above steps:
+    sum_of_all_num = average(arr[0..count-2]) * (count-1) + arr[count-1]
+    avg = sum_of_all_num / count
   end
-  avg
 end 
 
 p average([1]) == 1

9034141817bf86944299a3ef6a7ca9865bcb6afa
E <effyverse@gmail.com>
2022-02-01 14:31:39 -0500

    complete iterative solution to check for balanced parenthesis

---
+++ other/recursion/if_balanced_iterative.rb
@@ -0,0 +1,31 @@
+# Write a method that accepts a string containing open or closed parenthesis and returns true if the string has a balanced set of parenthesis. Use recursion if possible.
+
+# First implement via iteration
+def is_balanced?(string)
+  return false if string.empty?   
+    arr = string.chars.delete_if { |s| s == "," }
+  if arr.count > 0 && arr.count.odd?
+    false
+  else 
+    left = []
+    arr.each do |bracket|
+      if bracket == "("
+        left << bracket
+        arr.delete(bracket)
+      end
+    end
+    left.count == arr.count ? true : false
+  end
+end
+
+p is_balanced?("") == false 
+p is_balanced?("(,(,(") == false
+is_balanced?("(,(,(,),),)") == true
+
+p is_balanced?("(,)") == true
+p is_balanced?("(,(,(,),)") == false
+p is_balanced?("(") == false 
+p is_balanced?("(,(,(,(,(") == false
+
+
+

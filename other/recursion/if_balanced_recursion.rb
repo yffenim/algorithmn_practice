4383fa6795d6d92b11c93137046a7d6b4991b880
E <effyverse@gmail.com>
2022-02-01 17:32:25 -0500

    write tests for the recursion for balanced parenthesis

--- other/recursion/if_balanced_recursion.rb
+++ other/recursion/if_balanced_recursion.rb
@@ -1,12 +1,37 @@
-#def compare(array)
+# The recursion function
+
+# def compare(array)
  # array.each_with_index do |n, i|
   #  if array[i] != array[i+1]
-   #   puts i
     #  array.delete_at(i)
      # array.delete_at(i+1)
-     # p "inside match pairs"
     #end
   #end
 #end
 
+# recursion for balanced parenthesis problem
 
+def is_balanced?(input)
+  return false if input.empty? 
+  
+  # if input is not an array, make it an array
+  # this validation exists because my recursion function call accepts an array later and not a string
+  input.class == Array ? input : input = input.chars 
+ 
+  count = input.count
+  return false if count.odd?
+
+  # base case:
+  if count == 0
+    true
+  else 
+    
+  end
+end
+
+p is_balanced?("") == false
+p is_balanced?("(") == false
+p is_balanced?("(,)") == true
+p is_balanced?("(,(,(,),)") == false
+p is_balanced?("(,(,(,(,(") == false
+p is_balanced?("(,(,(,),),)") == true

25de6265ca85591412557380292aab34e1a4c9e2
E <effyverse@gmail.com>
2022-02-05 10:37:22 -0500

    separate logic for the tree graph and the nodes so that each node inherits from the tree

--- other/tree/tree_graph.rb
+++ other/tree/tree_graph.rb
@@ -1,25 +1,52 @@
-class Graph
-  # allows us to access the value of value
-  attr_reader :value
+class Tree
+  # array of all nodes in the graph
+  @@nodes = []
+  
+  # read method for the class variable @@nodes
+  def self.all
+    @@nodes
+  end
 
-  # default root node for new tree graph with default value of 1
+  # sum of all values of nodes
+  def sum 
+  
+  end
+
+  # print the current Tree
+  def print 
+  end
+end 
+
+class Node < Tree
+  attr_accessor :value
+
+  # add new node instance to the nodes array
   def initialize(value)
     @value = value
+    # default root node for new tree graph with default value of 1
+    @@nodes << self
   end
 
-  def edges
-    "I am an edge"
-  end
+  #def edges
+   # "I am an edge"
+  #end
 
-  def inspect 
-    "Node value: #{value}"
-    # add ability to display all children
-  end
-
-  def sum
-  end
-
+  # debug method for displaying object attributes by just calling the object
+  # def inspect 
+    #"Node value: #{value}"
+  #end
 end
 
-test = Graph.new(1)
-p test.inspect
+one = Node.new(1)
+two = Node.new(2)
+
+p one
+p two
+
+p Tree.all
+p Tree.all.count
+#three = Tree.new(3)
+#four = Tree.new(4)
+#five = Tree.new(5)
+
+

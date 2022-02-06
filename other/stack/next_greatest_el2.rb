60afce8f403e8c219dc304b9f807914e172caded
E <effyverse@gmail.com>
2022-02-06 09:22:13 -0500

    rewrite greatest element helper to only return one n instead of array of n

--- other/stack/next_greatest_el2.rb
+++ other/stack/next_greatest_el2.rb
@@ -1,22 +1,20 @@
 
   def next_greater_element_helper(array)
-    result = []
+    result = nil
     array.each_with_index do |n, i|
       break if array[i+1] == nil
       if array[i] < array[i+1]
-        result << array[i+1]
-      else
-        result << -1
+        result = array[i+1]
+        break
       end
     end
-    result << -1
+    result
   end 
 
   def next_greater_element(array)
-    
     next_greater_element_helper(array)
   end
 
-p next_greater_element([2,4],[1,2,3,4]) == [3,-1]
-p next_greater_element([4,1,2],[1,3,4,2]) == [-1,3,-1]
+p next_greater_element([1,2,3,4]) == 2
+p next_greater_element([3,1,4,2]) == 4
 

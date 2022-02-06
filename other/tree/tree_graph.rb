448755b21fb0fd20c02eb23901e03de55c11b04e
E <effyverse@gmail.com>
2022-02-06 11:50:37 -0500

    add edges and parents to tree graph objects

--- other/tree/tree_graph.rb
+++ other/tree/tree_graph.rb
@@ -1,65 +1,65 @@
-class Tree
-  attr_accessor :sum
-
-  # array of all nodes in the graph
+class Tree 
+  attr_accessor :node, :edge, :parent
   @@nodes = []
-  
-  # read method for the class variable @@nodes
-  def self.all
-    @@nodes
-  end
-
-  # sum of all values of nodes
-  def sum
-    # default sum is 1 because we have 1 node 
-    sum = 1
-    # if there are more than 1 nodes
-    if @@nodes.count > 1
-      arr = []
-      @@nodes.each do | node |
-      arr << node.value 
-      sum = arr.sum
-      end
-    end
-    sum
-  end
-
-  # print the current Tree
-  def print 
-    @@nodes.each do | node |
-      pp node
-    end
-  end
-end 
-
-class Node < Tree
-  attr_accessor :value
-  
-  def initialize(value)
-    @value = value
+  # initialize with default root node 
+  def initialize(node=1, edge=0, parent=0)
+    @node = node 
+    @edge = edge
+    @parent = parent
     @@nodes << self
   end
 
-  def add_edges
-    "I am an edge"
+  def print_sum_all
+    arr = []
+    sum = 1 #default value of root node
+    @@nodes.each do | object |
+      arr << object.node 
+      sum = arr.sum 
+    end
+    p sum
+  end 
+  
+  def print_all
+    p "Current tree: #{@@nodes}"
   end
-
-  # debug method for displaying object attributes by just calling the object
-  # def inspect 
-    #"Node value: #{value}"
-  #end
 end
 
-one = Node.new(1)
-two = Node.new(2)
 
-p one.value
-p two.value
+class Node < Tree
 
-p Tree.new.sum
-Tree.new.print
-#three = Tree.new(3)
-#four = Tree.new(4)
-#five = Tree.new(5)
+  def initialize(node, edge=1, parent)
+    @node = node 
+    @edge = edge
+    @parent = parent
+    node_is_new?
+    @@nodes << self
+    print_all
+  end
+
+  def node_is_new?
+    # check if node already exists
+    @@nodes.each do |n|
+      if n.node == @node
+      p "Node already exists!"
+      exit
+      end
+    end
+  end
+
+
+  def parent_exists?
+    # validate parent node  is correct
+  end
+
+  def parent_available?
+    # check if parent already has more than two nodes
+  end
+
+
+end
+
+tree = Tree.new
+# node1 =  Node.new(1,1,1)
+node2 =  Node.new(2,1,1)
 
 

bd973804d958b772d637546ccad54e88da0a87db
E <effyverse@gmail.com>
2022-02-10 07:36:41 -0500

    add comments to ensure that every line is doing as expected

--- other/tree/tree_count_nodes2.rb
+++ other/tree/tree_count_nodes2.rb
@@ -1,13 +1,20 @@
-  
+# Binary Tree from Leetcode
+# Implementing Count
+
 class TreeNode
+# set read and write access to the val, right, and left instances of the TreeNode object thereby making them mutable and accessible outside of the class
   attr_accessor :val, :left, :right
+
+# initialize an object of TreeNode class with the params val, left, and right set to default values
   def initialize(val = 0, left = nil, right = nil)
+# save the params into instance variables that can be used in TreeNode class
     @val = val
     @left = left
     @right = right
   end
 end
 
+# Initialize nodes of a new tree
 node_one = TreeNode.new(1,nil,nil)
 node_two = TreeNode.new(2,nil,nil)
 node_three = TreeNode.new(3,nil,nil)
@@ -16,6 +23,7 @@ node_five = TreeNode.new(5,nil,nil)
 node_six = TreeNode.new(6,nil,nil)
 node_seven = TreeNode.new(7,nil,nil)
 
+# Define edge between nodes of the tree
 node_one.left = node_two
 node_one.right = node_three
 
@@ -25,3 +33,5 @@ node_two.right = node_six
 node_three.right = node_four
 
 node_six.left = node_seven
+
+

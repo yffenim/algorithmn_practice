a824f6ec2ff0848f7a471357d708bb2247fc4cc6
E <effyverse@gmail.com>
2022-02-10 09:51:08 -0500

    init new tree for method of comparing two trees and write tests

---
+++ other/tree/is_same_tree3.rb
@@ -0,0 +1,51 @@
+class TreeNode
+  attr_accessor :val, :left, :right
+  def initialize(val = 0, left = nil, right = nil)
+    @val = val
+    @left = left
+    @right = right
+  end
+end
+
+# example 1 tree
+tree1node1 = TreeNode.new(1, nil, nil)
+tree1node2 = TreeNode.new(2, nil, nil)
+tree1node3 = TreeNode.new(3, nil, nil)
+tree2node1 = TreeNode.new(1, nil, nil)
+tree2node2 = TreeNode.new(2, nil, nil)
+tree2node3 = TreeNode.new(3, nil, nil)
+
+tree1node1.left = tree1node2
+tree1node1.right = tree1node3
+
+tree2node1.left = tree2node2
+tree2node1.right = tree2node3
+
+# example 2 tree 
+tree3node1 = TreeNode.new(1, nil, nil)
+tree3node2 = TreeNode.new(2, nil, nil)
+tree4node1 = TreeNode.new(1, nil, nil)
+tree4node2 = TreeNode.new(2, nil, nil)
+
+tree3node1.left = tree3node2 
+
+tree4node1.right = tree4node2
+
+# example 3 tree
+tree5node1 = TreeNode.new(1, nil, nil)
+tree5node2 = TreeNode.new(2, nil, nil)
+tree5node3 = TreeNode.new(1, nil, nil)
+tree6node1 = TreeNode.new(1, nil, nil)
+tree6node2 = TreeNode.new(2, nil, nil)
+tree6node3 = TreeNode.new(1, nil, nil)
+
+tree5node1.left = tree5node2
+tree5node1.right = tree5node3
+
+tree6node1.left = tree6node3
+tree6node1.right = tree6node2
+
+# tests
+p is_same_tree(tree1node1,tree2node1) == true
+p is_same_tree(tree2node1,tree3node1) == false
+p is_same_tree(tree4node1,tree5node1) == false

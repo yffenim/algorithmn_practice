1f73edd5c5e27659949e12823aeee85b20be5528
E <effyverse@gmail.com>
2022-02-13 19:45:38 -0500

    add recursive sum

--- dive/sum_of_n_threeways.rb
+++ dive/sum_of_n_threeways.rb
@@ -3,12 +3,37 @@
 
 # Iteration
 
-def sum(n)
+def sum_iter(n)
   result = 0
   (0..n).each do | i |
-    i += n 
+    result += i
+  end
+  result
+end
+
+p sum_iter(10) == 55
+
+# Mathematical
+# What makes this more math than the others?
+def sum_math(n)
+  return n * (n+1) / 2
+end
+
+p sum_math(10) == 55
+
+
+# Recursion
+# simplest case: sum(0) = 0
+# sum_rec(0) is the base case because at that point, 
+# sum_rec(5) = sum_rec(4) + 5
+# f(n) = f(n-1) + n
+
+def sum_rec(n) 
+  if n == 0
+    return 0
+  else
+  sum = sum_rec(n-1) + n
   end
 end
 
-p sum(10) 
-
+p sum_rec(10) == 55

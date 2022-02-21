6e2dd19a9fcbb851e38d56033d4d2508d2ce5540
E <effyverse@gmail.com>
2022-02-21 12:48:46 -0500

    add logic for finding smallest unit of closed brackets

--- other/recursion/reverse_string_inside_balanced_p.rb
+++ other/recursion/reverse_string_inside_balanced_p.rb
@@ -1,1 +1,27 @@
-new_string = string.reverse[1...-1]
+# new_string = string.reverse[1...-1]
+#
+def reverse_smallest_unit(string)
+  # iterate through
+  # find the first 
+  array = string.split("")
+
+  pointer1 = 0
+  pointer2 = 0
+
+  array.each_with_index do | char, i |
+    if char == "("
+      pointer1 = i 
+    end 
+    if char == ")"
+      pointer2 = i
+    break
+    end 
+  end
+  p pointer1 
+  p pointer2
+    
+end
+
+find_unit("((123)4)")
+
+

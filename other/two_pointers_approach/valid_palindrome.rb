6fb19186065e77d83ac2f60213bfd779a3941dd3
E <effyverse@gmail.com>
2022-02-22 08:04:24 -0500

    complete valid palindrome with two pointers without using reverse

--- other/two_pointers_approach/valid_palindrome.rb
+++ other/two_pointers_approach/valid_palindrome.rb
@@ -13,17 +13,27 @@ def is_palindrome?(s)
   # remove all non-alphanumeric charaacters and spaces and make lowercase
   # set pointers and match
   # logic for even/odd
- 
-  new_string = s.gsub!(/[^a-zA-Z0-9]/,"").downcase
-  # new_string.each_chars
-  # length = new_string.size
-  pointer1 = 0
-  pointer2 = new_string.size - 1
-  
+  return true if s.empty? || s.size == 1
 
+  new_string = s.gsub(/[^a-zA-Z0-9]/,"").downcase
+  array =  new_string.chars
+  pointer1 = 0
+  
+  while pointer1 < array.size/2 
+    if array[pointer1] == array[array.size-1] 
+      pointer2 = array.size-1
+      array.delete_at(pointer2)
+      array.delete_at(pointer1)
+      return true if array.empty? || array.size == 1
+    else 
+      return false
+    end
+  end
 end
 
-p is_palindrome?("6%a$sdf_fd-sa!6")
-p is_palindrome?("A man, a plan, a canal: Panama")
-# is_palindrome()
-# is_palindrome()
+p is_palindrome?("6%a$sdf_fd-sa!6") == true
+p is_palindrome?("A man, a plan, a canal: Panama") == true
+p is_palindrome?("abcba*#@^") == true 
+p is_palindrome?("aaba_") == false
+p is_palindrome?("") == true
+p is_palindrome?("aaba_") == false

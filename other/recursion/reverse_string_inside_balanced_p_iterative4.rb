91e000d225aed07b4dac3c47d1de9071db46c5fb
E <effyverse@gmail.com>
2022-02-23 17:41:16 -0500

    complete iterative solution using a temporary reverse array insode of the iterative loop

--- other/recursion/reverse_string_inside_balanced_p_iterative4.rb
+++ other/recursion/reverse_string_inside_balanced_p_iterative4.rb
@@ -2,17 +2,23 @@ def reverse_string_iterative(string)
   stack = [] 
   array = string.chars 
   array.each do | char | 
-    stack << char 
     if char == ")"
-      puts "stack is: #{stack}
+      reverse = []
+      until stack.last == "("
+        reverse << stack.pop
+      end
+      stack.pop
+      stack += reverse 
+    else
+      stack << char
     end
   end
+  stack.join
 end
     
-p reverse_string_iterative("((abcd)(dcba))")
-
-# p reverse_string_iterative("()") == ""
-# p reverse_string_iterative("(ab)") == "ba"
-# p reverse_string_iterative("(cd)(ab)") == "dcba"
-# p reverse_string_iterative("((cd)(ab))") == "abcd"
-# p reverse_string_iterative("(a(bc)((de))f)") == "fedbca"
+p reverse_string_iterative("((abcd)(dcba))") == "dcbaabcd"
+p reverse_string_iterative("()") == ""
+p reverse_string_iterative("(ab)") == "ba"
+p reverse_string_iterative("(cd)(ab)") == "dcba"
+p reverse_string_iterative("((cd)(ab))") == "abcd"
+p reverse_string_iterative("(a(bc)((de))f)") == "fedbca"

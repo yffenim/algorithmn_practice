784bb59af802e9eb0315663fc5027efa49307e8b
E <effyverse@gmail.com>
2022-02-24 08:18:59 -0500

    add tests for next greatest element

--- leetcode/integerbreak.rb
+++ leetcode/integerbreak.rb
@@ -9,6 +9,8 @@
 def integer_break(n)
   return 0 if n == 0 
   return 1 if n <= 2 
+  return 2 if n == 3
+
   product = nil 
 
   product = n/3 * n/3 * n/3 if n%3 == 0
--- other/stack/next_greatest_el5.rb
+++ other/stack/next_greatest_el5.rb
@@ -1,9 +1,18 @@
-# create a hash map of nums2 
-
 
 def next_greatest_el(nums2)
 
+  
+
+
+
+
 end
 
-p next_greater_el([4,1,2],[1,3,4,2]) == [-1,3,-11]
-p next_greater_el([2,4],[1,2,3,4]) == [3,-1]
+p next_greatest_el([4,1,2],[1,3,4,2]) == [-1,3,-11]
+p next_greatest_el([2,4],[1,2,3,4]) == [3,-1]
+p next_greatest_el([1,2,3,4],[9,7,8,1,6,5,2,4,3] == [6,4,-1,-1]
+p next_greatest_el([],[]) == [] 
+p next_greatest_em([],[1]) == []
+p next_greatest_em([1],[]) == [-1]
+p next_greatest_em([1],[2]) == [2]
+p next_greatest_em([2],[1]) == [-1]

a5fb36382d85e84d1032538191c7725680bd84d7
E <effyverse@gmail.com>
2022-02-24 10:10:46 -0500

    complete stack implementation though is it good questionable

--- other/stack/next_greatest_el5-stackimplemented.rb
+++ other/stack/next_greatest_el5-stackimplemented.rb
@@ -1,13 +1,12 @@
 
-def next_greatest_el(nums2)
-  # return [] if nums1.empty?
+def next_greatest_el(nums1,nums2)
+  return [] if nums1.empty?
   
   result = []
-  # return result += ([-1] * nums1.count) if nums2.empty?
+  return result += ([-1] * nums1.count) if nums2.empty?
   
   stack = [-1]
-  hash = {}
-  # fill the stack with the intended values of the hash map
+  
   last = -1 
   secondlast = -2
 
@@ -17,7 +16,7 @@ def next_greatest_el(nums2)
     # if n is not greater than n prior, then check if it is the current max
     if nums2[last] < nums2[secondlast]
       # n is the current max so there is no greater n after, add -1
-      if nums2[secondladdst] > stack.max
+      if nums2[secondlast] > stack.max
         stack << -1
       # n is not the current max so find the next greatest el
       else nums2[secondlast] < stack.max
@@ -32,19 +31,27 @@ def next_greatest_el(nums2)
     last -= 1
     secondlast -= 1
   end
-  stack
+
+  # intialize hash with nums2 array and stack array as k:v 
+  hash = nums2.zip(stack.reverse).to_h
+  
+  # iterate through nums1, replace elements with values from hash
+  # wtf @ this map here vs each here???
+  nums1 = nums1.map do |e|
+    hash.fetch(e,e)
+  end
+  nums1
 end
 
-p next_greatest_el([9,7,8,1,6,5,2,4,3]) == [-1,-1,4,-1,-1,6,-1,8,-1]
-p next_greatest_el([2,7,3,1,4,5,6,9,8]) == [-1,-1,9,6,5,4,4,9,7]
+# p next_greatest_el([9,7,8,1,6,5,2,4,3]) == [-1,-1,4,-1,-1,6,-1,8,-1]
+# p next_greatest_el([2,7,3,1,4,5,6,9,8]) == [-1,-1,9,6,5,4,4,9,7]
 
-# p next_greatest_el([4,1,2],[1,3,4,2]) == [-1,3,-11]
-# p next_greatest_el([2,4],[1,2,3,4]) == [3,-1]
-# p next_greatest_el([1,2,3,4],[9,7,8,1,6,5,2,4,3] == [6,4,-1,-1]
+p next_greatest_el([4,1,2],[1,3,4,2]) == [-1,3,-1]
+p next_greatest_el([2,4],[1,2,3,4]) == [3,-1]
+p next_greatest_el([1,2,3,4],[9,7,8,1,6,5,2,4,3]) == [6,4,-1,-1]
 
-# p next_greatest_el([],[]) == [] 
-# p next_greatest_el([],[1]) == []
-# p next_greatest_el([1],[]) == [-1]
-# p next_greatest_em([1],[1]) == [-1]
-# p next_greatest_el([1,2,3,4],[]) == [-1,-1,-1,-1]
-# p next_greatest_em([1],[2]) == [-1]
+p next_greatest_el([],[]) == [] 
+p next_greatest_el([],[1]) == []
+p next_greatest_el([1],[]) == [-1]
+p next_greatest_el([1],[1]) == [-1]
+p next_greatest_el([1,2,3,4],[]) == [-1,-1,-1,-1]

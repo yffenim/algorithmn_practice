e56efe40be9e971fa8933e8039c2b680e28a2a62
E <effyverse@gmail.com>
2022-02-24 08:27:12 -0500

    add logic to return result array if there is an empty first or second input array

--- other/stack/next_greatest_el5.rb
+++ other/stack/next_greatest_el5.rb
@@ -1,5 +1,11 @@
 
-def next_greatest_el(nums2)
+def next_greatest_el(nums1,nums2)
+  return [] if nums1.empty?
+  
+  result = []
+  return result += ([-1] * nums1.count) if nums2.empty?
+  
+  hash = {}
 
   
 
@@ -8,11 +14,13 @@ def next_greatest_el(nums2)
 
 end
 
-p next_greatest_el([4,1,2],[1,3,4,2]) == [-1,3,-11]
-p next_greatest_el([2,4],[1,2,3,4]) == [3,-1]
-p next_greatest_el([1,2,3,4],[9,7,8,1,6,5,2,4,3] == [6,4,-1,-1]
+# p next_greatest_el([4,1,2],[1,3,4,2]) == [-1,3,-11]
+# p next_greatest_el([2,4],[1,2,3,4]) == [3,-1]
+#p next_greatest_el([1,2,3,4],[9,7,8,1,6,5,2,4,3] == [6,4,-1,-1]
+
 p next_greatest_el([],[]) == [] 
-p next_greatest_em([],[1]) == []
-p next_greatest_em([1],[]) == [-1]
-p next_greatest_em([1],[2]) == [2]
-p next_greatest_em([2],[1]) == [-1]
+p next_greatest_el([],[1]) == []
+p next_greatest_el([1],[]) == [-1]
+#p next_greatest_em([1],[1]) == [-1]
+p next_greatest_el([1,2,3,4],[]) == [-1,-1,-1,-1]
+# p next_greatest_em([1],[2]) == [-1]

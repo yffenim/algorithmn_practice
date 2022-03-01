2ff0f304812c9e56bd3726a4cb010844ac7b6b63
E <effyverse@gmail.com>
2022-03-01 10:55:06 -0500

    add AssertionError class that inherits from RuntimeError class and define method to raise error if block returns false

--- hackerrank/rot13.rb
+++ hackerrank/rot13.rb
@@ -1,11 +1,20 @@
+class AssertionError < RuntimeError
+end
+
+def assert &block
+  raise AssertionError unless yield
+end
+
 def rot13(secret_messages)
     words = secret_messages.split
     new = words.map do | w | 
-        w.tr("abcdefghijklmnopqrstuvwxyz", "nopqrstuvwxyzabcdefghijklm")
+        w.tr('A-Za-z','N-ZA-Mn-za-m')
     end
     new.join(" ")
 end
 
-p rot13("I am a secret")
-p rot13("Why did the chicken cross the road?") == "Jul qvq gur puvpxra pebff gur ebnq?"
-p rot13("Gb trg gb gur bgure fvqr!") == "To get to the other side!"
+
+assert { rot13("Why did the chicken cross the road?") == "Jul qvq gur puvpxra pebff gur ebnq?" }
+assert { rot13("Gb trg gb gur bgure fvqr!") == "To get to the other side!" }
+# assert { rot13("raise error") == "raise error" }
+

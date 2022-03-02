228b1346726189530236503261908afac532d893
E <effyverse@gmail.com>
2022-03-02 08:16:55 -0500

    add permutations outline

---
+++ other/permutation/next_permutation.rb
@@ -0,0 +1,18 @@
+# Input: an array of intergers nums 
+# Output: next permutation of nums 
+# Definitions: the next permutation of an array of integers is the next "lexicographically greater permutation of its integer > it is the next one in the sorted array of possible permutations
+# Instructions:
+# Methods:
+# Edges Cases:
+
+def next_permutation(nums)
+  
+end
+
+
+p next_permutation([1,2,3]) == [1,3,2]
+p next_permutation([3,2,1]) == [1,2,3]
+p next_permutation([1,1,5]) == [1,5,1]
+p next_permutation([0) == [0]
+p next_permutation([1]) == [1]
+p next_permutation([1,1,1]) == [1,1,1]

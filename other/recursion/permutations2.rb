1fb7bb53cbe7d28f39e1074e5ebcfea1ea9f84c2
E <effyverse@gmail.com>
2022-03-06 16:03:16 -0500

    second attempt at permutations fail

--- other/recursion/permutations2.rb
+++ other/recursion/permutations2.rb
@@ -1,9 +1,30 @@
 
 
-def permute(options, start=0, result=[])
-  result << options[0]
+def permute(options, result=[], tracker=[true, true, true], temp=[])
+  return result if tracker.all? { |x| x == false }
 
+  for i in (0...options.length) do 
+    if tracker[i] == true
+      puts "inside loop"
+      temp << options[i]
+      tracker[i] = false
+      # puts "tracker: #{tracker}"
+    elsif tracker[i] == false
+    end
+  end
+  
+  if temp.count == 3 
+    result << temp
+    puts "result: #{result}"
+    temp = [] 
+    last = options.shift
+    options << last
+    tracker=[false,true,true]
+  end
+  puts "options: #{options}"
+
+  permute(options, result, tracker, temp)
 end
 
 
-permuate([1,2,3])
+p permute([1,2,3])

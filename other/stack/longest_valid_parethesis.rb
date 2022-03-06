e0c709586c9914f80c0eb450346f41ba718901b8
E <effyverse@gmail.com>
2022-03-06 12:05:45 -0500

    add failed implementation

--- other/stack/longest_valid_parethesis.rb
+++ other/stack/longest_valid_parethesis.rb
@@ -6,29 +6,44 @@
 # if ")" and stack.last == "("
 #   check if stack is empty? 
 #   if empty: add i bc you are at a lonely bracket
-#   if not empty: pop last, call max
+#   if not empty: pop last, call max, add i
 
 def longest_valid_parentheses(s)
   return 0 if s.length < 2
   
   stack = [-1]
-  max = 0 
+  max = []
 
-  s.each_with_index do | bracket, i |
+  s.chars.each_with_index do | bracket, i |
     if bracket == "("      
-      stack << i 
+      stack << i
+      # puts "stack is: #{stack}"
+    elsif bracket == ")" && s[i-1] == "("
+      stack.pop
+      puts "stack is: #{stack}"
+      max << find_max(stack, i)
+      stack << i
+      puts "the max #{max}"
     else 
-     # check for empty
+      stack << i
     end
-  max 
+  end
+  puts max
 end
 
-p longest_valid_parentheses("())(())()") == 6
-p longest_valid_parentheses("()(())") == 6
-p longest_valid_parentheses("(((())))") == 8
-p longest_valid_parentheses("()(())") == 6
-p longest_valid_parentheses("") == 0
-p longest_valid_parentheses("()") == 2
-p longest_valid_parentheses("(()") == 2 
-p longest_valid_parentheses(")()())") == 4
-p longest_valid_parentheses(")))))") == 0
+def find_max(stack, right_bracket)
+  # puts "right: #{right_bracket} and stack: #{stack.last}"
+  max = right_bracket - stack.last
+end
+
+
+
+p longest_valid_parentheses("())(())()")
+# p longest_valid_parentheses("()(())") == 6
+# p longest_valid_parentheses("(((())))") == 8
+# p longest_valid_parentheses("()(())") == 6
+# p longest_valid_parentheses("") == 0
+# p longest_valid_parentheses("()") == 2
+# p longest_valid_parentheses("(()") == 2 
+# p longest_valid_parentheses(")()())") == 4
+# p longest_valid_parentheses(")))))") == 0

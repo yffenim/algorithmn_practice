ac593bf1c71d8abadcaff577691a3ecbf1dc2129
E <effyverse@gmail.com>
2022-03-07 10:27:54 -0500

    add the read adn write capacity for the left andn right attributes of tree nodes or else you will not be able to access the attributes as public methods" "

--- other/stack/binary_tree_preorder_stack.rb
+++ other/stack/binary_tree_preorder_stack.rb
@@ -1,33 +1,29 @@
-# Tree object definition
+# define a binary tree with each node having attributes of a value, left node, and right node.
+# the tree has a known root to start
 class BinaryTree 
-  attr_accessor :root 
   
-  # Initialize a Binary Tree with given root value
   def initialize(root)
     @root = root 
-  end 
-
-  def preorder_traversal(root)
-    puts "inside the pre!"
   end
-
 
 end
 
-# Tree Node object definition
+
 class TreeNode
-  attr_accessor :val, :left, :right
-  def initialize(val = 0, left = nil, right = nil)
+  attr_accessor :val, :left, :right 
+  def initialize(val=nil, left=nil, right=nil)
+    # counter
     @val = val
     @left = left
     @right = right
   end
+
 end
 
-# Initialize a root node 
-root = TreeNode.new(1,nil,nil)
+
 
 # Construct the nodes of the rest of the three
+root = TreeNode.new(1, nil, nil)
 node2 = TreeNode.new(2,nil,nil)
 node3 = TreeNode.new(3,nil,nil)
 node4 = TreeNode.new(4,nil,nil)
@@ -39,7 +35,6 @@ root.right = node3
 node2.left = node4
 node2.right = node5 
 
-# Initialize a Binary Tree 
-tree = BinaryTree.new(root)
 
-p tree.preorder_traversal(root)
+tree = BinaryTree.new(root)
+p new
